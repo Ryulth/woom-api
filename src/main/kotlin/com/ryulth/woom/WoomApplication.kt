e9e1e791a4b2e0319f426ab1c7475028b38fbe34
jeju <jeju@hpcnt.com>
2020-02-12 14:51:51 +0900

    Init project

---
+++ src/main/kotlin/com/ryulth/woom/WoomApplication.kt
@@ -0,0 +1,13 @@
+package com.ryulth.woom
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@SpringBootApplication
+@EnableSwagger2
+class WorkLifeBellApplication
+
+fun main(args: Array<String>) {
+    runApplication<WorkLifeBellApplication>(*args)
+}
